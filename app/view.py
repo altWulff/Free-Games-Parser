f312bf7d0199ff684b28c3593de683441fec5af5
Oleg Repalo <altwulf7@gmail.com>
2022-05-08 18:55:08 +0300

    Add docstring

--- app/view.py
+++ app/view.py
@@ -1,3 +1,7 @@
+"""
+Base renders games
+"""
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -11,6 +15,9 @@ templates = Jinja2Templates(directory="app/templates")
 
 @routes.get("/", response_class=HTMLResponse)
 async def index(request: Request):
+    """
+    Request json and return html
+    """
     api_request = await request_games()
     return templates.TemplateResponse(
         "index.html", {"request": request, "data": api_request, "base_url": BASE_URL}
