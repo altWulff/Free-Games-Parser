4bc5e955bfee911ef9ba8729f6d95cb6ede8fc63
Oleg Repalo <altwulf7@gmail.com>
2022-05-09 11:42:26 +0300

    Add moment js to index.html

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 app.include_router(routes)
 app.include_router(api_router, prefix="/api/v1")
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+#app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
