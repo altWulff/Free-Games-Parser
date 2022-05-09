97f1fd5afcf6b9b55cc0c5894f3a2da95fed3f1b
Oleg Repalo <altwulf7@gmail.com>
2022-05-09 20:53:50 +0300

    Add schema GameCard, and new endpoint game_by_id

--- app/api/api_v1/endpoints.py
+++ app/api/api_v1/endpoints.py
@@ -7,7 +7,7 @@ import requests
 from fastapi import APIRouter
 
 from app.config import URL
-
+from app.schemas import GameCard
 api_router = APIRouter()
 
 
@@ -20,3 +20,16 @@ async def games() -> list[dict]:
     request = requests.get(URL)
     data = request.json()
     return data["data"]["Catalog"]["searchStore"]["elements"]
+
+
+@api_router.get('/{game_id}')
+async def game_by_id(game_id: int) -> GameCard:
+    """
+    Id game request
+    :param game_id: int
+    :return: GameCard
+    """
+    request = requests.get(URL)
+    data = request.json()
+    data = data["data"]["Catalog"]["searchStore"]["elements"][game_id]
+    return GameCard(**data)
---
+++ app/schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+
+class GameCard(BaseModel):
+    id: str
+    title: str
+    namespace: str
+    description: str
+    effectiveDate: str
+    offerType: str
+    expiryDate: None
+    status: str
+    isCodeRedemptionOnly: bool
+    keyImages: list[dict]
+    seller: dict
+    productSlug: str
+    urlSlug: str
+    url: None
+    items: list[dict]
+    customAttributes: list[dict]
+    categories: list[dict]
+    tags: list[dict]
+    catalogNs: dict
+    offerMappings: list
+    price: dict
+    promotions: None
