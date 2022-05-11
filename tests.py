b4f3eaf7ed82632587e046e16ed1460a78f2044c
Oleg Repalo <altwulf7@gmail.com>
2022-05-11 16:16:06 +0300

    Add docstrings on tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,9 @@
+"""
+Base tests
+"""
+
+
+
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -6,10 +12,16 @@ client = TestClient(app)
 
 
 def test_main():
+    """
+    Response root endpoint
+    """
     response = client.get("/")
     assert response.status_code == 200
 
 
 def test_api_response():
+    """
+    Response api endpoint
+    """
     response = client.get("/api/v1/")
     assert response.status_code == 200
