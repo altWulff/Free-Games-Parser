4817c64a1f9b63e04934e517b99f1a1a46fb2e3b
Oleg Repalo <altwulf7@gmail.com>
2022-05-12 14:33:33 +0300

    Update tests.py, and add verbose arg to pytest

--- tests.py
+++ tests.py
@@ -3,7 +3,6 @@ Base tests
 """
 
 
-
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -13,7 +12,7 @@ client = TestClient(app)
 
 def test_main():
     """
-    Response root endpoint
+    Test response root endpoint
     """
     response = client.get("/")
     assert response.status_code == 200
@@ -21,7 +20,31 @@ def test_main():
 
 def test_api_response():
     """
-    Response api endpoint
+    Test response api endpoint
     """
     response = client.get("/api/v1/")
+    response_data = response.json()
     assert response.status_code == 200
+    assert len(response_data) > 0
+
+
+def test_api_response_data_len():
+    """
+    Test response data
+    assert length greater than one
+    """
+    response = client.get("/api/v1/")
+    response_data = response.json()
+    assert len(response_data) > 0
+
+
+def test_api_response_correct_schema():
+    """
+    Test response data
+    assert correct data in schema
+    """
+    response = client.get("/api/v1/")
+    response_data = response.json()
+    for indx, _ in enumerate(response_data):
+        response = client.get(f"/api/v1/{indx}")
+        assert response.status_code == 200
